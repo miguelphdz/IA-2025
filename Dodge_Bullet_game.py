2a9bd33192e41539f1898d72fd43f4ab8df6d6f2
Miguel Ponce Hernandez <miguelhernandez353@outlook.com>
2025-09-11 08:00:11 -0600

    Dodge bullet game fundamentals

---
+++ Dodge_Bullet_game.py
@@ -0,0 +1,24 @@
+#Necessary variables
+
+# 1. Person (starts at the center)
+# 2. Boundaries
+# 3. Guns
+# 4. Guns Position
+# 5. gun angle of shooting
+# 6. bullet speed
+# 7. person movement speed
+# 8. movement coordinates
+
+#To learn
+
+# 1. Player position in map
+# 2. Distance from the human to the bullets
+# 3. if the player is in bullet trajectory
+
+#maybe a reward system
+
+# 1.time survied
+# 2.penalty for impact
+
+
+
